045c2d464026a4b4dba09e41f5b59250bf29990c
bxljoy <259500313@qq.com>
2022-08-13 13:50:02 +0800

    convert videos from webn to mp4

---
+++ dailyApp/AudioExtract2.py
@@ -0,0 +1,21 @@
+import os
+
+def convertWebmToMp4(path):
+    cmd = '/Users/bxl/Downloads/ffmpeg -i '
+    # suffix = ' -crf 23 '
+    for file in os.listdir(path):
+        if file.split('.')[-1] not in ['webm']:
+            continue
+        newFile = path + file
+        print(newFile)
+        newSuffix = file.split('.')[0] + '.mp4'
+        print(newSuffix)
+        # finalCmd = cmd + '\"' + newFile + '\"' + suffix + '\"' + path + newSuffix + '\"'
+        finalCmd = cmd + '\"' + newFile + '\" ' + ' \"' + path + newSuffix + '\"'
+        print(finalCmd)
+        os.system(finalCmd)
+
+if __name__ == '__main__':
+    path = '/Users/bxl/Downloads/LankyBox13/'
+    convertWebmToMp4(path)
+
